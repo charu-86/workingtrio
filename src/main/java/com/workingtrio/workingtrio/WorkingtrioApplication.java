204737ca69e492f73b3af69adb7dbf8d02c7a88a
charu-86 <kashyapcharu20@gmail.com>
2024-08-10 13:33:20 +0530

    initial commit

---
+++ src/main/java/com/workingtrio/workingtrio/WorkingtrioApplication.java
@@ -0,0 +1,13 @@
+package com.workingtrio.workingtrio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingtrioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingtrioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workingtrio/workingtrio/WorkingtrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workingtrio.workingtrio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingtrioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
