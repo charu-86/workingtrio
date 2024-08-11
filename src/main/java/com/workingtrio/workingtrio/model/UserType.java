b5b9a06354b0f0f66f2acabd832739a844000d8d
tiwariaditya89 <adityakumartiwari564@gmail.com>
2024-08-11 10:53:54 +0530

    Added configuration table

--- src/main/java/com/workingtrio/workingtrio/model/UserType.java
+++ src/main/java/com/workingtrio/workingtrio/model/UserType.java
@@ -3,11 +3,18 @@ package com.workingtrio.workingtrio.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+import java.io.Serializable;
+
+
 @Entity
-public class UserType {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserType implements Serializable {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
